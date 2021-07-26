48b0c07a109d55d4fd0fbbf34c30e7a92a2ea2df
xu <2803106301@qq.com>
2021-07-26 22:48:29 +0800

    first commit

---
+++ src/main/java/com/xu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.xu.git;
+
+/**
+ * @Author: ZeXu
+ * @CreateTime: 2021-07-2021/7/26-26-22:37
+ * @Explain：
+ */
+
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello ok?");
+    }
+}
