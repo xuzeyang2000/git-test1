63af9c6a08dbbdf1637f92e137bf3b0dd4099aca
xuzeyang2000 <82041640+xuzeyang2000@users.noreply.github.com>
2021-07-27 01:19:44 +0800

    pull commit

--- src/main/java/com/xu/git/GitTest.java
+++ src/main/java/com/xu/git/GitTest.java
@@ -15,5 +15,7 @@ public class GitTest {
         System.out.println("hello arr you not ok  in master?");
 
         System.out.println("push test");
+        System.out.println("web test   pull test");
+        
     }
 }
